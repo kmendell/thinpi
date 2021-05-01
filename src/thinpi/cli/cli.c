a6ba74cc45dd75215928d437eb3aca976f1bd573
ThinPi (KM) <getthinpi@gmail.com>
2021-04-30 20:55:39 -0500

    v2 Update

--- src/thinpi/cli/cli.c
+++ src/thinpi/cli/cli.c
@@ -13,17 +13,7 @@ void main (int argc, char *argv[])
     //this is failing in this build.... no idea why the argv wont pass into the other if statments.....
     sleep(2);
     if( argc == 2 ) {
-        if ( argv[2] == '-r' ) {
-        printf("Reset all config files in the system\n");
-        } else if (argv[1] == 'install') {
-        char pkg[100];
-        char cmd[100];
-
-        printf("Package to install: \n");
-        gets( pkg );
-        sprintf(cmd, "sudo apt-get install %s", pkg);
-        system(cmd);
-        }
+        
    }
    else if( argc > 2 ) {
         printf("Too many arguments supplied.\n");
