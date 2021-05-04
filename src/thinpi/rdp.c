d5ad81c8c8c3098abdbbdcf5ff14f531674c3af0
Kyle Mendell <admin@kmprojects.us>
2021-05-04 17:22:23 +0000

    RDP Code Cleanup / RDP Arg additions

--- src/thinpi/rdp.c
+++ src/thinpi/rdp.c
@@ -6,9 +6,8 @@
 void openConnection(gchar *u, gchar *p, gchar *s)
 {	
 	char *cmd = malloc(100);
-	sprintf(cmd, "xfreerdp +gfx-thin-client /cert-ignore /cert-tofu /size:1920x1080 /f /gdi:hw /smart-sizing:1920x1080 /u:'%s' /p:'%s' /v:%s", u, p, s);
+	sprintf(cmd, "xfreerdp +home-drive +gfx-thin-client /t:ThinPi /cert:tofu /size:1280x720 /f /rfx /audio:sys:alsa /gdi:sw /u:'%s' /p:'%s' /v:%s", u, p, s);
 	int rv = system(cmd);
-	printf("%s\n", cmd);
 	if(rv == 0 || rv == 2816) {
 		gtk_entry_set_text(usernameTextbox, "");
 		gtk_entry_set_text(passwordTextbox, "");
