80f6a3610dbf72ca56ef2f0ac4e2a41470e68fae
Kyle Mendell <admin@kmprojects.us>
2021-05-04 20:18:23 +0000

    Update addserver.c to add remove function, not yet tested

--- src/thinpi/addserver.c
+++ src/thinpi/addserver.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../include/thinpi.h"
 
 gchar *ipToAdd;
@@ -46,5 +47,68 @@ gtk_window_present(configWindow);
 gtk_main();
 
 }
+
+int removeServer()
+{
+    FILE * fPtr;
+    FILE * fTemp;
+    char path[100];
+    
+    char toRemove[100];
+    char buffer[1000];
+
+
+    /* Input source file path path */
+    path = "/thinpi/config/servers"
+
+    printf("Enter word to remove: ");
+    toRemove = gtk_entry_get_text (ipaddressTextbox);
+
+
+    /*  Open files */
+    fPtr  = fopen(path, "r");
+    fTemp = fopen("delete.tmp", "w"); 
+
+    /* fopen() return NULL if unable to open file in given mode. */
+    if (fPtr == NULL || fTemp == NULL)
+    {
+        /* Unable to open file hence exit */
+        printf("\nUnable to open file.\n");
+        printf("Please check whether file exists and you have read/write privilege.\n");
+        exit(EXIT_SUCCESS);
+    }
+
+
+    /*
+     * Read line from source file and write to destination 
+     * file after removing given word.
+     */
+    while ((fgets(buffer, BUFFER_SIZE, fPtr)) != NULL)
+    {
+        // Remove all occurrence of word from current line
+        removeAll(buffer, toRemove);
+
+        // Write to temp file
+        fputs(buffer, fTemp);
+    }
+
+
+    /* Close all files to release resource */
+    fclose(fPtr);
+    fclose(fTemp);
+
+
+    /* Delete original source file */
+    remove(path);
+
+    /* Rename temp file as original file */
+    rename("delete.tmp", path);
+
+
+    printf("\nServer '%s' has been removed successfully.", toRemove);
+
+    return 0;
+}
+
 
 
